a333814f5f9725715853e6ac7ff0bdd2772e04c1
k.Jeswanth Reddy <103727742+jeswanthreddy2002@users.noreply.github.com>
2023-02-09 15:01:01 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.cpp
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int arr[],
+                                             long long int n, long long int k);
+
+// Driver program to test above functions
+int main() {
+    long long int t, i;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        long long int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        long long int k;
+        cin >> k;
+
+        vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+        for (auto it : ans) cout << it << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+                                                   
+            queue<long long >q;
+            long long i=0;
+            long long j=0;
+            vector<long long >v;
+            while(j<N)
+            {
+                if(j-i+1<K)
+                {
+                    if(A[j]<0)
+                    {
+                        q.push(A[j]);
+                    }
+                    j++;
+                }
+                if(j-i+1==K)
+                {  if(A[j]<0)
+                {
+                    q.push(A[j]);
+                }
+                   
+                     j++;
+                if(!q.empty())
+                {
+                    v.push_back(q.front());
+                }
+                else{
+                    v.push_back(0);
+                }
+                    if(!q.empty()&&A[i]==q.front())
+                    {
+                    q.pop();
+                    }
+                
+                    i++;
+                }
+                
+            }
+                                                 
+             return v;                                    
+ }
+ 
+ 
+ 
+ 
+
+// vector<long long> printFirstNegativeInteger(long long int A[],
+//                                              long long int N, long long int K) {
+                                                   
+//             queue<long long >q;
+//             long long i=0;
+//             long long j=0;
+//             vector<long long >v;
+//             while(j<N)
+//             {
+//                 if(j-i+1<K)
+//                 {
+//                     if(A[j]<0)
+//                     {
+//                         q.push(A[j]);
+//                     }
+//                     j++;
+//                 }
+//                 if(j-i+1==K)
+//                 {  if(A[j]<0)
+//                 {
+//                     q.push(A[j]);
+//                 }
+//                 if(!q.empty())
+//                 {
+//                     v.push_back(q.front());
+//                 }
+//                 else{
+//                     v.push_back(0);
+//                 }
+//                     if(!q.empty()&&A[i]==q.front())
+//                     {
+//                     q.pop();
+//                     }
+//                     j++;
+//                     i++;
+//                 }
+                
+//             }
+                                                 
+//              return v;                                    
+//  }
