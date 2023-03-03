2b958832f1bbe93031ba55f28aaa76aa637c83e2
k.Jeswanth Reddy <103727742+jeswanthreddy2002@users.noreply.github.com>
2023-03-03 13:28:46 +0530

    Added solution - LeetHub

---
+++ Stack Permutations - GFG/stack-permutations.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        stack<int>st;
+        int k=0;
+        
+        for(int i=0;i<A.size();i++)
+        { 
+            st.push(A[i]);
+            while(!st.empty()&&st.top()==B[k])
+            {
+                st.pop();
+                k++;
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        Solution ob;
+        cout<<ob.isStackPermutation(n,a,b)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
